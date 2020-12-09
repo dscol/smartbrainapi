60b8dfcdcc333c29ff64bc2d23527b6e852ee167
Derek <derekscolnick@gmail.com>
2020-12-09 17:50:53 +1000

    register outside

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import knex from 'knex';
 import bcrypt from 'bcrypt';
-import handleRegister from './controllers/register.js';
+//import handleRegister from './controllers/register.js';
 import handleSignIn from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
 import image from './controllers/image.js';
@@ -27,11 +27,42 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => handleSignIn(req, res, bcrypt, db));
-app.post('/register', (req, res) => handleRegister(req, res, bcrypt, saltRounds, db));
+//app.post('/register', (req, res) => handleRegister(req, res, bcrypt, saltRounds, db));
 app.get('/profile/:id', (req, res) => handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    
+    if (!email || !name || !password) {
+        return res.status(400).json('incorrect form submission')
+    }
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`)
