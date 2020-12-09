ff7daa3f40af0c15cf4d5cb29326dfd08baa219b
Derek <derekscolnick@gmail.com>
2020-12-09 18:05:02 +1000

    register back inside

--- controllers/register.js
+++ controllers/register.js
@@ -1,33 +1,35 @@
-// const handleRegister = (req, res, bcrypt, saltRounds, db) => {
-//     const { name, email, password } = req.body;
-//     const hash = bcrypt.hashSync(password, saltRounds);
-    
-//     if (!email || !name || !password) {
-//         return res.status(400).json('incorrect form submission')
-//     }
-//     db.transaction(trx => {
-//         trx.insert({
-//           hash: hash,
-//           email: email
-//         })
-//         .into('login')
-//         .returning('email')
-//         .then(loginEmail => {
-//           return trx('users')
-//             .returning('*')
-//             .insert({
-//               email: loginEmail[0],
-//               name: name,
-//               joined: new Date()
-//             })
-//             .then(user => {
-//               res.json(user[0]);
-//             })
-//         })
-//         .then(trx.commit)
-//         .catch(trx.rollback)
-//       })
-//       .catch(err => res.status(400).json('unable to register'))
-// }
+const handleRegister = (req, res, bcrypt, db) => {
+    const saltRounds = 10;
+    const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    console.log(name, email, password, hash);
 
-// export default handleRegister;
+    if (!email || !name || !password) {
+        return res.status(400).json('incorrect form submission')
+    }
+    db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+      .catch(err => res.status(400).json('unable to register'))
+}
+
+export default handleRegister;
