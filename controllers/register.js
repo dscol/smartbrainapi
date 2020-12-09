fe884c99a73dbb08fa61129e037e54459c3aa371
Derek <derekscolnick@gmail.com>
2020-12-09 23:00:36 +1000

    register error text fix

--- controllers/register.js
+++ controllers/register.js
@@ -28,7 +28,7 @@ const handleRegister = (req, res, bcrypt, db) => {
         .then(trx.commit)
         .catch(trx.rollback)
       })
-      .catch(err => res.status(400).json('unable to register')
+      .catch(err => res.status(400).json('unable to register'))
 }
 
 export default handleRegister;
